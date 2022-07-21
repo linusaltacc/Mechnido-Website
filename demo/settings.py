31685bd2c33ae66f0825a69bf0b5ae384cb953b8
Sunil <linusaltacc01@protonmail.com>
2022-07-21 05:34:51 +0530

    deploying for heroku

--- demo/settings.py
+++ demo/settings.py
@@ -2,10 +2,10 @@ import os
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','mechnido.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- templates/raw-html/README.md
+++
@@ -1,4 +0,0 @@
-# 
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/Priyankajeeva/pen/NWYddYg](https://codepen.io/Priyankajeeva/pen/NWYddYg).
-
